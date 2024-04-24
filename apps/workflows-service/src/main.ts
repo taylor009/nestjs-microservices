45d0d3248b7e1c19f0c461685cad7e801e101fb8
taylor <taylor@baremetalcloud.com>
2024-04-23 23:32:59 -0700

    Add queue to NATS options in workflows-service
    A queue has been added to the NATS options within the workflows-service. This change helps keep track of tasks and will manage the distribution of tasks among multiple services better.

--- apps/workflows-service/src/main.ts
+++ apps/workflows-service/src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
       transport: Transport.NATS,
       options: {
         servers: process.env.NATS_URL,
+        queue: 'workflows-service',
       },
     },
     { inheritAppConfig: true },
